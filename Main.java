e419758e1341e0ea35fada03b5e4e3b4e7fad6d4
jt474 <tranjohn474@yahoo.com>
2020-11-23 17:51:54 -0800

    update menu

--- Main.java
+++ Main.java
@@ -28,11 +28,13 @@ public class Main {
 				}
 			}
 			boolean mainMenu = true;
-			boolean invoiceMenu = true;
+			//boolean invoiceMenu = true;
+			//boolean warehouseMenu = true;
 			while (mainMenu) {
 				System.out.print( "What do you want to do? \n1. Invoices \n2. Warehouses \n3. Products \n4. SalesPerson \n5. Change Password \n6. Exit \n" );
 			    int menuChoice = CheckInput.getIntRange(1, 6);
 			    if (menuChoice == 1){
+			    	boolean invoiceMenu = true;
 			    	while(invoiceMenu) {
 			    		System.out.println("What do you want to do? \n1. Create Invoice \n2. Edit Invoice \n3. Go back \n");
 			    		int invoiceMenuChoice = CheckInput.getIntRange(1,3);
@@ -50,11 +52,34 @@ public class Main {
 			    	}
 			    }
 			    if (menuChoice == 2){
-			    	System.out.println("What do you want to do? \n1. Add Warehouse \n2. Edit Warehouse Stock \n3. Go back \n");
+			    	boolean warehouseMenu = true;
+			    	while(warehouseMenu) {
+			    		System.out.println("What do you want to do? \n1. Add Warehouse \n2. Remove Warehouse \n3. Go back \n");
+			    		int warehouseMenuChoice = CheckInput.getIntRange(1,3);
+			    		if(warehouseMenuChoice == 1) {
+			    			System.out.println("Adding new warehouse...");
+			    			//Add warehouse
+			    		}
+			    		if(warehouseMenuChoice == 2) {
+			    			System.out.println("Which warehouse do you want to remove?");
+			    			//Remove warehouse
+			    		}
+			    		if(warehouseMenuChoice == 3) {
+			    			warehouseMenu = false;
+			    		}
+			    	}
 			    }
 			    
 			    if (menuChoice == 3) {
 			    	System.out.println("1. Add new Product \n2. Edit Product Quantity \n3. Display all products \n4. Go back \n ");
+			    }
+			    
+			    if (menuChoice == 4) {
+			    	System.out.println("1. Add new Salesperson \n2. Remove Salesperson \n3. Go back \n ");
+			    }
+			    
+			    if (menuChoice == 5) {
+			    	System.out.println("1. Change password \n2. Go back \n ");
 			    }
 
 			    if (menuChoice == 6){
